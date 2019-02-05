69c69d57c9d8ea679993867b5a4bee43dfd16593
propersam <a.samuel@suretrade.ng>
2019-02-05 12:42:50 +0100

    rearrange settings files for production

---
+++ auth_service/settings/development.py
@@ -0,0 +1,39 @@
+from auth_service.settings.common import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '(k)!m@k7aadsj27o@)vqb1s!93x$fksq7+m!6_r@1x%7%4l*bq'
+
+# Allowed hosts (list of comma-separated host names, or asterisk to match all hosts), only needed if DEBUG is false
+ALLOWED_HOSTS = ['testserver', '*']
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+	    'NAME': 'sureedu_auth',
+	    'USER': 'sureedu',
+	    'PASSWORD': 'sureedu',
+	    'HOST': 'db',
+	    'PORT': '5432',
+	    'DEFAULT-CHARACTER-SET': 'utf8',
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+    # 'default': {
+    #     'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+    # },
+}
---
+++ auth_service/settings/production.py
@@ -0,0 +1,43 @@
+from auth_service.settings.common import *
+
+DEBUG = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '(k)!m@k7aadsj27o@)vqb1s!93x$fksq7+m!6_r@1x%7%4l*bq')
+
+# Allowed hosts (list of comma-separated host names, or asterisk to match all hosts), only needed if DEBUG is false
+ALLOWED_HOSTS = ['testserver', '*']
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+         'DEFAULT-CHARACTER-SET': 'utf8',
+        'NAME': 'sureeduauth',
+        'USER': 'sureedu',
+        'PASSWORD': 'sureedu',
+        'HOST': 'db', # OR any host for the database
+        'PORT': 5432,
+        'TEST': {
+            'NAME': 'testdatabase',
+        },
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    },
+    # 'default': {
+    #     'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    # },
+}
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
